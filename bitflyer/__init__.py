b7751f36bab0735b802f4ae74a168adde36d3089
Shohei KAMON <kamonshohei@gmail.com>
2016-02-04 16:04:06 +0900

    modify version

--- bitflyer/__init__.py
+++ bitflyer/__init__.py
@@ -4,8 +4,8 @@ current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(current_dir,'../bitflyer'))
 
 __title__ = 'bitflyer'
-__version__ = '0.0.1a'
+__version__ = '0.0.1'
 __author__ = 'Shohei Kamon'
 __license__ = 'MIT license'
-__copyright__ = 'Copyright 2015 - 2016 Shohei Kamon'
+__copyright__ = 'Copyright 2016 Shohei Kamon'
 
